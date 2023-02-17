cdd7b301d3c5d358527ec476f3beb372a269cde4
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-02-17 14:59:35 +0100

    failing the app
    failing the app

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ mongoose.connect(process.env.MONGO_URL!);
 mongoose.connection.on("connected", () => {
   httpServer.listen(port, () => {
     console.log("listening");
-    // DO NOT FORGET TO LISTEN WITH HTTPSERVER HERE, NOT EXPRESS SERVER!!
     console.table(listEndpoints(expressServer));
     console.log(`Server is running on port ${port}`);
   });
